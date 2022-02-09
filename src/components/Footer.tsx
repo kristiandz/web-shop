aefaeb5b5028bd1161513540d45ad9295cf5f90c
Kristian Džidžić <kristian334@gmail.com>
2022-02-09 20:20:36 +0100

    Footer component & style

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return ( 
+        <div className={styles.footer}>
+            <span>© TINEL Meetup 2022.</span>
+        </div>
+     );
+}
+
+export default Footer;
