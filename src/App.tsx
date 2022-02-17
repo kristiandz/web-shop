b83c9addf3331dcb99cd2e049d12de586fd22e5f
Kristian Džidžić <kristian334@gmail.com>
2022-02-17 18:12:51 +0100

    Application logic
    - Implemented routing
    - Basic authorization logic from the code only !
    - Wrapped the authorized components with cart context
    
    TODO: Authorization to be able to login/logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { CartContextProvider } from "./store/cart-context";
+import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Orders from "./pages/Orders";
+// Global CSS settings for the application, in the rest of the appliaction we are using local css modules and classNames
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+  const [isAuthorized, setAuthorize] = useState(false); // Authorization state, default is false
+
+  useEffect(() => {
+    if (isAuthorized === false)
+      setAuthorize(true);
+    else
+      setAuthorize(false);
+    // eslint-disable-next-line 
+  }, []);
+
+  // Depending on the state, the app is rendering different pages
+  if (isAuthorized) {
+    return (
+      // Provide context for the application when signed in by wrapping components with the context wrapper
+      // If the route is not matched, render the ErrorPage
+      <CartContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </CartContextProvider>
+    );
+  }
+  else {
+    return (
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    );
+  }
 }
 
 export default App;
