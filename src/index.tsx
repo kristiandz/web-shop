368443a89b30128f3d9babd0f357aa1fa8722905
Kristian Džidžić <kristian334@gmail.com>
2022-02-17 14:18:59 +0100

    Update index.tsx
    Adding comments

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
+// Get the root div element from the html and start building our appliaction from here.
+// Wrap the appliaction in BrowserRouter so we can use history API, and using StrictMode to display more potential probles and errors.
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
