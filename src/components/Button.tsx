ab74f920f9ba8d2838c4d3c1194d1c24c52bb19d
Kristian Džidžić <kristian334@gmail.com>
2022-02-17 14:20:53 +0100

    Button component implemented
    - Custom button component which accepts different props to achieve reuse across the whole application
    - If no props are passed it retains default style and inherits the width of the text.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 import styles from "./Button.module.css";
 
-function Button(props:any) {
-    // The width can be set from the parent, otherwise its default (the width of the text for the button)
+function Button(props: any) {
+    // The styles can be set from the parent, otherwise it is using default values in css
     return (
-        // Using inline style for simplicity
-        <button type="button" className={styles.button} style={{width:props?.width}}>{props.title}</button>
+        // Using inline style for simplicity, although it is not recommended. This way we can simply reuse the component across the website.
+        <button
+            type="button"
+            className={styles.button}
+            style={{ width: props?.width, height: props?.height, margin: props?.margin, background: props?.background, color: props?.color }}>
+            {props.title}
+        </button>
     );
 }
 
