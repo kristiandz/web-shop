1ecc012eef45ed7fe6ca77d770b05a6c095212ab
Kristian Džidžić <kristian334@gmail.com>
2022-02-17 16:24:02 +0100

    WorkshopCard component
    - Using the passed data from Home to render the workshop card
    - CET time format to split the date string from DB, and en-GB for the right order of dd/mm/yyyy
    - Checking the category icon from CategoryRow.tsx via the exported helper function

---
+++ src/components/WorkshopCard.tsx
@@ -0,0 +1,57 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faClock, faCalendarTimes } from '@fortawesome/free-regular-svg-icons'
+import { getCategoryIcon } from "./CategoryRow";
+import { useContext } from "react";
+import CartContext from "../store/cart-context";
+import Button from "./Button";
+import styles from "./WorkshopCard.module.css";
+
+function WorkshopCard(props: any) {
+
+    const cartContext = useContext(CartContext);
+    const defaultImage = "https://secure.meetupstatic.com/photos/event/2/d/8/e/highres_482651662.jpeg";
+
+    const addToCart = () => {
+        if (!cartContext.isInCart(props?.value.id)) { // Is the workshop already in the cart ?
+            let workshop = props?.value; // Assign the workshop prop to new var
+            workshop.amount = 1; // Add a quantity property so we can easily calculate the total price later on
+            cartContext.addToCart(workshop); // Add the workshop to the context state
+        }
+    }
+
+    // Parse the date from db
+    let data = new Date(props?.value.date);
+    let time = data.toLocaleString('en-GB', { timeZone: 'CET' });
+
+    return (
+        <div className={styles.workshopCard}>
+            {/* Use the default image in case we don't have a value from passed props */}
+            <div className={styles.workshopCard__startContainer}>
+                <img className={styles.workshopCard__image} src={props.value.imageUrl ? props.value.imageUrl : defaultImage} alt="IMG" />
+                {/* Get the icon from CategoryRow.tsx to reduce component size */}
+                <div className={styles.workshopCard__categoryIcon}>
+                    <FontAwesomeIcon icon={getCategoryIcon(props?.value.category)} size="sm" color="white" />
+                </div>
+            </div>
+            <div className={styles.workshopCard__infoContainer}>
+                <div className={styles.workshopCard__time}>
+                    <FontAwesomeIcon icon={faCalendarTimes} />
+                    <span>{time.split(",")[0]}.</span>
+                    <FontAwesomeIcon icon={faClock} />
+                    <span>{time.split(",")[1].substring(0, 6)}h</span>
+                </div>
+                <h3>{props?.value.title}</h3>
+                {/* Hardcoding the currency here, since we don't have a value to fetch from database */}
+                <div className={styles.workshopCard__price}>
+                    <h2>{props?.value.price}</h2> <span>EUR</span>
+                </div>
+            </div>
+            {/* Wrapping the button in a div so we can use the onClick callback, we are adding the workshop to the cart here by passing the data to the context state */}
+            <div onClick={addToCart}>
+                <Button title="Add to Cart" width="14vw" margin="2vh" />
+            </div>
+        </div>
+    );
+}
+
+export default WorkshopCard;
